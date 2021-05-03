74f3bebe7c67e1c8f71c1dae6ab8582793a947c1
jameshadleyg <jwh129@scarletmail.rutgers.edu>
2021-05-03 00:37:19 -0400

    added activity for viewing a replay, replays now loads data from save. Missing implementation of content_show_replay.xml as well as handling in ShowReplay.java

---
+++ app/src/main/java/com/example/chess/Replays.java
@@ -0,0 +1,86 @@
+package com.example.chess;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.chess.chessBoard.Replay;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Replays extends AppCompatActivity {
+
+    private ListView listView;
+    private ArrayList<Replay> replays;
+
+    public static final String REPLAY_NAME = "replay_name";
+    public static final String REPLAY_DATE = "replay_date";
+    public static final String REPLAY_MOVES = "replay_moves";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_replays);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Populate ListView
+        // Get Replay array
+
+        String address = getFilesDir() + System.getProperty("file.separator") + "games.txt";
+
+        try {
+            FileInputStream fis = openFileInput(address);
+            BufferedReader br = new BufferedReader(
+                    new InputStreamReader(fis));
+
+            String line;
+            replays = new ArrayList<Replay>();
+            while ((line = br.readLine()) != null) {
+                String[] tokens = line.split("\\|");
+                replays.add(new Replay(tokens));
+            }
+        } catch (IOException e) {
+            replays = new ArrayList<Replay>();
+        }
+
+        // Set up adapter and listView
+        ArrayAdapter<Replay> adapter =
+                new ArrayAdapter<>(this, R.layout.content_replays, replays);
+
+        listView = findViewById(R.id.replays_list);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener((parent, view, position, id) -> showReplay(position));
+    }
+
+    private void showReplay(int pos) {
+        Bundle bundle = new Bundle();
+
+        Replay replay = (Replay)listView.getItemAtPosition(pos);
+
+        bundle.putString(REPLAY_NAME, replay.getName());
+        bundle.putString(REPLAY_DATE, replay.getDate());
+        bundle.putStringArray(REPLAY_MOVES, replay.getMoves());
+
+        Intent intent = new Intent(this, ShowReplay.class);
+        intent.putExtras(bundle);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/chess/ShowReplay.java
@@ -0,0 +1,66 @@
+package com.example.chess;
+
+import android.os.Bundle;
+
+import com.example.chess.chessBoard.*;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import java.util.ArrayList;
+
+public class ShowReplay extends AppCompatActivity {
+
+    ArrayList<Board> configurations;
+    int currIndex = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_replay);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Get bundle data
+
+        Bundle bundle = getIntent().getExtras();
+        String name = bundle.getString(Replays.REPLAY_NAME);
+        String date = bundle.getString(Replays.REPLAY_DATE);
+        String[] moves = bundle.getStringArray(Replays.REPLAY_MOVES);
+
+        // Load configurations
+        configurations = new ArrayList<Board>();
+        BoardState boardState = new BoardState();
+
+        configurations.add(boardState.board.copy());
+
+        String turn = "w";
+        for (String move:moves) {
+            String[] positions = move.split(" ");
+
+            if(positions.length == 3) {
+                Piece p = boardState.getPromotion(positions[2], turn);
+                boardState.setPosition(positions[0], positions[1], p, turn);
+            }
+            else {
+                Piece p = boardState.getPromotion(" ", turn);
+                boardState.setPosition(positions[0], positions[1], p, turn);
+            }
+
+            configurations.add(boardState.board.copy());
+
+            if (turn.equals("w")) turn = "b";
+            else turn = "w";
+        }
+
+        // Display configurations
+
+    }
+}
